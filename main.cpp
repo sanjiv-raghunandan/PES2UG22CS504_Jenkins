f41964f6e6fb8ec33e3dc43ba616fbe62068a24d
Sanjiv Raghunandan <141476056+sanjiv-raghunandan@users.noreply.github.com>
2025-03-12 20:57:46 +0530

    main.cpp jheeze

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello from Jenkins Pipeline!" << std::endl;
+    std::cout << "This is a test program for CI/CD demonstration" << std::endl;
+    return 0;
+}
